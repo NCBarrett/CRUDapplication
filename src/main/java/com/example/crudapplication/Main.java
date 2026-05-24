2bcbea022ba753e10927e138eddc8b8a01cd7109
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2026-05-24 12:08:39 -0700

    Created GUI. Solved problem with customer.css

---
+++ src/main/java/com/example/crudapplication/Main.java
@@ -0,0 +1,16 @@
+package com.example.crudapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+    public class CRUDapplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setScene(new Scene(new CustomerController().getView()));
+        primaryStage.show();
+    }
+}
